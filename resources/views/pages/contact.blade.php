b190a1b069a734164b06773c8f6bc40795546df5
lookittah <ajh59@njit.edu>
2018-02-24 19:19:24 -0500

    Procfile for Heroku

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.default')
+
+@section('content')
+    <div class="card mt-3 pl-2 pr-2">
+        <div class="card-title"><h1> contact</h1>
+
+            <p class="lead">Please use this form to contact the site owner.</p>
+        </div>
+        <div class="card-body"></div>
+
+        <form action="/contact" method="post">
+            {{ csrf_field() }}
+
+            <div class="form-group">
+                <label for="email">Email address</label>
+                <input name="email" type="email" class="form-control" id="exampleFormControlInput1"
+                       placeholder="name@example.com">
+            </div>
+
+            <div class="form-group">
+                <label for="body"> Message</label>
+                <textarea name="body" class="form-control" id="body" rows="3"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary mb-2">Submit</button>
+        </form>
+    </div>
+    </div>
+@endsection
+
